e02ba3eb3b4cea33e562a2c93e2ece71cb135004
hyebin <tpointt@gmail.com>
2021-01-17 01:56:51 +0900

    commit practice 210117_2

---
+++ main2.py
@@ -0,0 +1,40 @@
+# patterns = ['가위','보','보','가위','가위']
+# for pattern in patterns:
+#     print(pattern)
+#
+
+#
+# for i in range(5):
+#     print(i)
+
+
+names = ['reina','jeff','jinny','joy']
+
+# for i in range(4):
+#     name = names[i]
+#     print('{}번: {}'.format(i+1, name))
+
+
+# for i in range(len(names)):
+#     name = names[i]
+#     print('{}번: {}'.format(i+1, name))
+
+#
+# for i, name in enumerate(names):
+#     print('{}번: {}'.format(i+1, name))
+
+# for i in range(11172):
+#     print(chr(44032+i), end='')
+
+
+# def get_web(url):
+#     import urllib.request
+#     response = urllib.request.urlopen(url)
+#     data = response.read()
+#     decoded = data.decode('utf-8')
+#     return decoded
+#
+# url = input ('웹 페이지 주소? ')
+# content = get_web(url)
+# print(content)
+
