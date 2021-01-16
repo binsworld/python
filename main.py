4933b3e7c38afedfc4adf7c028d089a10945ad2f
hyebin <tpointt@gmail.com>
2021-01-17 01:23:25 +0900

    commit practice 210117

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-number = 20
-greeting = '안녕하세요'
-place = '스무디'
-welcome = '환영합니다'
+# patterns = ['가위','보','보','가위','가위']
+# for pattern in patterns:
+#     print(pattern)
+#
 
-base = '{}번 손님, {}. {}에 오신 것을 {}!'
-new_way = base.format(number, greeting, place, welcome)
-#print(base)
-print(new_way)
+#
+# for i in range(5):
+#     print(i)
+
+
+names = ['reina','jeff','jinny','joy']
+
+# for i in range(4):
+#     name = names[i]
+#     print('{}번: {}'.format(i+1, name))
+
+
+# for i in range(len(names)):
+#     name = names[i]
+#     print('{}번: {}'.format(i+1, name))
+
+#
+# for i, name in enumerate(names):
+#     print('{}번: {}'.format(i+1, name))
+
+for i in range(11172):
+    print(chr(44032+i), end='')
