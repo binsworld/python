344da2c9f32e74762bb53616bab9d26b9ecc311e
hyebin <tpointt@gmail.com>
2021-01-23 23:34:28 +0900

    commit practice 210123

---
+++ logic.py
@@ -0,0 +1,27 @@
+# a = 10
+# if a<0 and 2**a > 1000 and a%5 == 2 and round(a) == a:
+#     print("복잡한 식")
+
+
+# def return_false():
+#     print("함수 return_false")
+#     return False
+#
+# def return_true():
+#     print("함수 return_true")
+#     return True
+#
+# print("테스트1")
+# a = return_false()
+# b = return_true()
+# if a and b:
+#     print(True)
+# else:
+#     print(False)
+#
+# print("테스트2")_
+# if return_false() and return_true():
+#     print("True")
+# else:
+#     print(False)
+
---
+++ while.py
@@ -0,0 +1,22 @@
+# selected = None
+# while selected not in ['가위','바위','보']:
+#     selected = input('가위,바위, 보 중에 선택하세요>')
+# print ('선택된 값은: ',selected)
+
+# patterns = ['가위','보','보']
+# for pattern in patterns:
+#     print(pattern)
+
+# patterns = ['가위','보','보']
+# for i in range(len(patterns)):
+#     print(patterns[i])
+
+patterns = ['가위','보','부']
+length = len(patterns)
+i=0
+while i < length:
+    print(patterns[i])
+    i=i+1
+
+
+
